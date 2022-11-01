a922f0940035d8c8fcfe26fb9e2e299093394370
light <lightseraph@163.com>
2022-11-01 20:23:49 +0800

    11.1.1

--- Core/Inc/rtc.h
+++ Core/Inc/rtc.h
@@ -1,52 +1,59 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    rtc.h
-  * @brief   This file contains all the function prototypes for
-  *          the rtc.c file
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    rtc.h
+ * @brief   This file contains all the function prototypes for
+ *          the rtc.c file
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __RTC_H__
 #define __RTC_H__
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* USER CODE BEGIN Includes */
+  /* USER CODE BEGIN Includes */
+#include "sys.h"
+  /* USER CODE END Includes */
 
-/* USER CODE END Includes */
+  extern RTC_HandleTypeDef hrtc;
 
-extern RTC_HandleTypeDef hrtc;
+  /* USER CODE BEGIN Private defines */
 
-/* USER CODE BEGIN Private defines */
+  /* USER CODE END Private defines */
 
-/* USER CODE END Private defines */
+  void MX_RTC_Init(void);
 
-void MX_RTC_Init(void);
-
-/* USER CODE BEGIN Prototypes */
-
-/* USER CODE END Prototypes */
+  /* USER CODE BEGIN Prototypes */
+  u8 RTC_Wait_Synchro(void);
+  HAL_StatusTypeDef RTC_Set_Time(u8 hour, u8 min, u8 sec, u8 ampm);    // RTC时间设置
+  HAL_StatusTypeDef RTC_Set_Date(u8 year, u8 month, u8 date, u8 week); // RTC日期设置
+  void RTC_Set_WakeUp(u32 wksel, u16 cnt);                             //周期性唤醒定时器设置
+  u8 RTC_DEC2BCD(u8 val);                                              //十进制转换为BCD码
+  u8 RTC_BCD2DEC(u8 val);                                              // BCD码转换为十进制数据
+  void RTC_Get_Time(u8 *hour, u8 *min, u8 *sec, u8 *ampm);
+  void RTC_Get_Date(u8 *year, u8 *month, u8 *date, u8 *week);
+  /* USER CODE END Prototypes */
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __RTC_H__ */
-
--- Core/Src/rtc.c
+++ Core/Src/rtc.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    rtc.c
-  * @brief   This file provides code for the configuration
-  *          of the RTC instances.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    rtc.c
+ * @brief   This file provides code for the configuration
+ *          of the RTC instances.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "rtc.h"
@@ -42,7 +42,7 @@ void MX_RTC_Init(void)
   /* USER CODE END RTC_Init 1 */
 
   /** Initialize RTC Only
-  */
+   */
   hrtc.Instance = RTC;
   hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
   hrtc.Init.AsynchPrediv = 127;
@@ -61,7 +61,7 @@ void MX_RTC_Init(void)
   /* USER CODE END Check_RTC_BKUP */
 
   /** Initialize RTC and set the Time and Date
-  */
+   */
   sTime.Hours = 0;
   sTime.Minutes = 0;
   sTime.Seconds = 0;
@@ -83,21 +83,20 @@ void MX_RTC_Init(void)
   /* USER CODE BEGIN RTC_Init 2 */
 
   /* USER CODE END RTC_Init 2 */
-
 }
 
-void HAL_RTC_MspInit(RTC_HandleTypeDef* rtcHandle)
+void HAL_RTC_MspInit(RTC_HandleTypeDef *rtcHandle)
 {
 
   RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
-  if(rtcHandle->Instance==RTC)
+  if (rtcHandle->Instance == RTC)
   {
-  /* USER CODE BEGIN RTC_MspInit 0 */
+    /* USER CODE BEGIN RTC_MspInit 0 */
 
-  /* USER CODE END RTC_MspInit 0 */
+    /* USER CODE END RTC_MspInit 0 */
 
-  /** Initializes the peripherals clock
-  */
+    /** Initializes the peripherals clock
+     */
     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
     PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
@@ -107,28 +106,132 @@ void HAL_RTC_MspInit(RTC_HandleTypeDef* rtcHandle)
 
     /* RTC clock enable */
     __HAL_RCC_RTC_ENABLE();
-  /* USER CODE BEGIN RTC_MspInit 1 */
+    /* USER CODE BEGIN RTC_MspInit 1 */
 
-  /* USER CODE END RTC_MspInit 1 */
+    /* USER CODE END RTC_MspInit 1 */
   }
 }
 
-void HAL_RTC_MspDeInit(RTC_HandleTypeDef* rtcHandle)
+void HAL_RTC_MspDeInit(RTC_HandleTypeDef *rtcHandle)
 {
 
-  if(rtcHandle->Instance==RTC)
+  if (rtcHandle->Instance == RTC)
   {
-  /* USER CODE BEGIN RTC_MspDeInit 0 */
+    /* USER CODE BEGIN RTC_MspDeInit 0 */
 
-  /* USER CODE END RTC_MspDeInit 0 */
+    /* USER CODE END RTC_MspDeInit 0 */
     /* Peripheral clock disable */
     __HAL_RCC_RTC_DISABLE();
-  /* USER CODE BEGIN RTC_MspDeInit 1 */
+    /* USER CODE BEGIN RTC_MspDeInit 1 */
 
-  /* USER CODE END RTC_MspDeInit 1 */
+    /* USER CODE END RTC_MspDeInit 1 */
   }
 }
 
 /* USER CODE BEGIN 1 */
+void RTC_Get_Time(u8 *hour, u8 *min, u8 *sec, u8 *ampm)
+{
+  u32 temp = 0;
+  while (RTC_Wait_Synchro())
+    ; //等待同步
+  temp = RTC->TR;
+  *hour = RTC_BCD2DEC((temp >> 16) & 0X3F);
+  *min = RTC_BCD2DEC((temp >> 8) & 0X7F);
+  *sec = RTC_BCD2DEC(temp & 0X7F);
+  *ampm = temp >> 22;
+}
 
+void RTC_Get_Date(u8 *year, u8 *month, u8 *date, u8 *week)
+{
+  u32 temp = 0;
+  while (RTC_Wait_Synchro())
+    ; //等待同步
+  temp = RTC->DR;
+  *year = RTC_BCD2DEC((temp >> 16) & 0XFF);
+  *month = RTC_BCD2DEC((temp >> 8) & 0X1F);
+  *date = RTC_BCD2DEC(temp & 0X3F);
+  *week = (temp >> 13) & 0X07;
+}
+
+u8 RTC_Wait_Synchro(void)
+{
+  u32 retry = 0XFFFFF;
+  //关闭RTC寄存器写保护
+  RTC->WPR = 0xCA;
+  RTC->WPR = 0x53;
+  RTC->ISR &= ~(1 << 5); //清除RSF位
+  while (retry && ((RTC->ISR & (1 << 5)) == 0x00))
+    retry--; //等待影子寄存器同步
+  if (retry == 0)
+    return 1;      //同步失败
+  RTC->WPR = 0xFF; //使能RTC寄存器写保护
+  return 0;
+}
+
+HAL_StatusTypeDef RTC_Set_Time(u8 hour, u8 min, u8 sec, u8 ampm) // RTC时间设置
+{
+  RTC_TimeTypeDef RTC_TimeStructure;
+
+  RTC_TimeStructure.Hours = hour;
+  RTC_TimeStructure.Minutes = min;
+  RTC_TimeStructure.Seconds = sec;
+  RTC_TimeStructure.TimeFormat = ampm;
+  RTC_TimeStructure.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+  RTC_TimeStructure.StoreOperation = RTC_STOREOPERATION_RESET;
+  return HAL_RTC_SetTime(&hrtc, &RTC_TimeStructure, RTC_FORMAT_BIN);
+}
+
+HAL_StatusTypeDef RTC_Set_Date(u8 year, u8 month, u8 date, u8 week) // RTC日期设置
+{
+  RTC_DateTypeDef RTC_DateStructure;
+
+  RTC_DateStructure.Date = date;
+  RTC_DateStructure.Month = month;
+  RTC_DateStructure.WeekDay = week;
+  RTC_DateStructure.Year = year;
+  return HAL_RTC_SetDate(&hrtc, &RTC_DateStructure, RTC_FORMAT_BIN);
+}
+
+//周期性唤醒定时器设置
+/*wksel:  @ref RTCEx_Wakeup_Timer_Definitions
+#define RTC_WAKEUPCLOCK_RTCCLK_DIV16        ((uint32_t)0x00000000)
+#define RTC_WAKEUPCLOCK_RTCCLK_DIV8         ((uint32_t)0x00000001)
+#define RTC_WAKEUPCLOCK_RTCCLK_DIV4         ((uint32_t)0x00000002)
+#define RTC_WAKEUPCLOCK_RTCCLK_DIV2         ((uint32_t)0x00000003)
+#define RTC_WAKEUPCLOCK_CK_SPRE_16BITS      ((uint32_t)0x00000004)
+#define RTC_WAKEUPCLOCK_CK_SPRE_17BITS      ((uint32_t)0x00000006)
+*/
+// cnt:自动重装载值.减到0,产生中断.
+void RTC_Set_WakeUp(u32 wksel, u16 cnt)
+{
+  __HAL_RTC_WAKEUPTIMER_CLEAR_FLAG(&hrtc, RTC_FLAG_WUTF); //清除RTC WAKE UP的标志
+
+  HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, cnt, wksel); //设置重装载值和时钟
+
+  HAL_NVIC_SetPriority(RTC_WKUP_IRQn, 0x02, 0x02); //抢占优先级1,子优先级2
+  HAL_NVIC_EnableIRQ(RTC_WKUP_IRQn);
+}
+
+//十进制转换为BCD码
+// val:要转换的十进制数
+//返回值:BCD码
+u8 RTC_DEC2BCD(u8 val)
+{
+  u8 bcdhigh = 0;
+  while (val >= 10)
+  {
+    bcdhigh++;
+    val -= 10;
+  }
+  return ((u8)(bcdhigh << 4) | val);
+}
+// BCD码转换为十进制数据
+// val:要转换的BCD码
+//返回值:十进制数据
+u8 RTC_BCD2DEC(u8 val)
+{
+  u8 temp = 0;
+  temp = (val >> 4) * 10;
+  return (temp + (val & 0X0F));
+}
 /* USER CODE END 1 */
